448a2867f5c2d1c5814cbe1ad5c8ff60d8e8b4dc
Michael Boulton <michaelboulton@gmail.com>
2021-11-06 15:40:25 +0000

    finish

--- generate.go
+++ generate.go
@@ -18,46 +18,34 @@ var genCmd = &cobra.Command{
 			return err
 		}
 
-		for i := 0; i < 10; i++ {
-			println(generated[i])
+		for i := 0; i < len(generated); i++ {
+			print(generated[i])
 		}
+		println()
 
 		return nil
 	},
 }
 
 func gen() ([]string, error) {
-	c := make(chan *sno.SequenceOverflowNotification)
-
 	const nPartitions = 10
 	const nTokens = 1000
 
-	generators := make([]*sno.Generator, nPartitions)
+	var generators []*sno.Generator
 	for i := 0; i < nPartitions; i++ {
-		b := make([]byte, 2)
-		_, err := rand.Reader.Read(b)
+		generator, err := makeGenerator()
 		if err != nil {
 			return nil, err
-		}
-
-		generator, err := sno.NewGenerator(&sno.GeneratorSnapshot{
-			Partition: sno.Partition{
-				b[0], b[1],
-			},
-		}, c)
-		if err != nil {
-			return nil, errors.Wrap(err, "making coder")
 		}
 
 		generators = append(generators, generator)
 	}
 
-
 	metabytes := make([]byte, nTokens)
 	times := make([]time.Time, nTokens)
 	now := time.Now()
 	for i := 0; i < nTokens; i++ {
-		times[i] = now.Add(time.Duration(i) * time.Second)
+		times[i] = now.Add(time.Duration(rand2.Int31n(100)) * time.Second)
 
 		read, err := rand.Reader.Read(metabytes[i : i+1])
 		if err != nil {
@@ -70,10 +58,16 @@ func gen() ([]string, error) {
 
 	tokens := make([]sno.ID, nTokens)
 	for i := 0; i < nTokens; i++ {
-		generator := generators[rand2.Int31n(int32(len(generators)))]
+		var generator *sno.Generator
+		for {
+			generator = generators[rand2.Int31n(int32(len(generators)))]
+			newToken := generator.NewWithTime(metabytes[i], times[i])
 
-		newToken := generator.NewWithTime(metabytes[i], times[i])
-		tokens[i] = newToken
+			if rand2.Int31n(100) < 5 {
+				tokens[i] = newToken
+				break
+			}
+		}
 	}
 
 	asString := make([]string, nTokens)
@@ -87,4 +81,23 @@ func gen() ([]string, error) {
 	}
 
 	return asString, nil
+}
+
+func makeGenerator() (*sno.Generator, error) {
+	b := make([]byte, 2)
+	_, err := rand.Reader.Read(b)
+	if err != nil {
+		return nil, err
+	}
+
+	c := make(chan *sno.SequenceOverflowNotification)
+	generator, err := sno.NewGenerator(&sno.GeneratorSnapshot{
+		Partition: sno.Partition{
+			b[0], b[1],
+		},
+	}, c)
+	if err != nil {
+		return nil, errors.Wrap(err, "making coder")
+	}
+	return generator, nil
 }
